94bbbb1d2921ee286c56b9e49dfd1b690bb8b150
glorijailieva <81261518+glorijailieva@users.noreply.github.com>
2021-05-07 19:19:34 +0200

    Add files via upload

---
+++ domasno.js
@@ -0,0 +1,40 @@
+let name = 'Glorija';
+let surName = 'Ilieva';
+let age = 20;
+ console.log(name);
+ console.log(surNAame);
+ console.log(age);
+
+ console.log(typeof name);
+ console.log(typeof surName);
+ console.log(typeof age);
+
+
+const pi = 3.14;
+console.log(pi);
+
+pi = 3.15;
+console.log(pi);
+
+
+let person1 = {
+    firstName='Melani',
+    lastName='Ristova',
+    age=20
+};
+ let peroson2={
+     firstName='Kika',
+     lastName='Krsteva',
+     age=21
+ };
+
+ let people = [person1, person2];
+  console.log(person1);
+  console.log(person2);
+  console.log(people);
+
+  console.log(typeof person1);
+  console.log(typeof person2);
+  console.log(typeof people);
+
+ 
