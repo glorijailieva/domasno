0aff9932800806159d6a37ed76800c5fa26aeeac
glorijailieva <81261518+glorijailieva@users.noreply.github.com>
2021-05-14 19:13:58 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,76 +1,92 @@
 
-
-let a = 2;
-let b = 5; 
-let c = 4;
-let d = 7;
-let e = '6';
-
-
-
-add = a + c + d;
-console.log(add);
-sub = b - c - d;
-console.log(sub);
-mult = b * c;
-console.log(mult);
-div = b / d;
-console.log(div);
-mod = a % c;
-console.log(mod);
-a++;
+let a = 3;
+let b = 6;
+let c =5;
+a = 4;
 console.log(a);
-a = a + 1;
+a += 1;
 console.log(a);
-b--;
+b += 3;
 console.log(b);
-b = b - 1;
+b -= 1;
+console.log(b);
+b *= 3;
+console.log(b);
+c /= 1;
+console.log(c);
+c %= 2;
+console.log(c);
+b %= 3;
 console.log(b);
 
 
+let x = 6;
+let y = 8;
+let z = 9;
 
-a = 2;
-b = 5;
-c = 4;
-d = 7;
-e = '6';
-console.log(b == e);
-console.log(a == b);
-console.log(b === e);
-console.log(b != e);
-console.log(b !== e);
-console.log(a !== d);
-console.log(a > d);
-console.log(a >= d);
-console.log(b < c);
-console.log(b <= a);
-
-
-
-console.log(a > c || c > b);
-console.log(a < b || d > c);
-console.log(b = e && a > d);
-
-console.log(d > a && c < b);
-console.log('a > c || c > b' ,!(a > c || c > b));
-console.log('d > a && c < b' , !(d > a && c < b));
+if (x <= y && y <= z) {
+    console.log(x, y, z);
+} else if (x <= z && z <= y) { 
+    console.log(x, z, y);
+} else if (y <= x && x <= z) {
+    console.log(y, x, z);
+}else if(y <= z && z <=x) {
+    console.log(y, z, x);
+}else if(z <= x && x <= y) {
+    console.log(z, x, y);
+} else  {
+    console.log(z, y, x);
+}
 
 
+let David = 80;
+let Vinoth = 77;
+let Divya = 88;
+let Ishitha = 95;
+let Tomash = 68;
+let prosec = (David + Vinoth + Divya + Ishitha + Tomash) / 5;
+console.log(prosec);
 
-console.error('This message is an error!');
-console.table(['day', 'month', 'year']);
-console.log(['day', 'month', 'year']);
-let fruit = { 
-    name: 'Apple',
-    color: "red",
-    form: "circular"
- };
-console.dir(fruit);
-console.dir(location); 
-console.log(location);
-console.info('Semos course: JavaScript basics.')
+if (prosec < 60) {
+    console.log('F');
+} else if(prosec < 70) {
+    console.log('D');
+} else if (prosec < 80) {
+    console.log('D');
+} else if (prosec < 90) {
+    console.log('B');
+} else if( prosec < 100) {
+    console.log('A');
+}
 
-console.time('script');
-setTimeout(() => {
-    console.timeEnd('script');
-}, 7000);
+
+let day = 'Cetvrtok';
+switch (day){
+    case 'Ponedelnik':
+        console.log('workDay');
+        break;
+    case 'Vtornik':
+        console.log('workDay');
+        break;
+    case 'Sreda':
+        console.log('workDay');
+        break;
+    case 'Cetvrtok':
+        console.log('workDay');
+        break;
+    case 'Petok':
+         console.log('workDay');
+        break;
+    case 'Sabota':
+        console.log('weekend');
+        break;
+     case 'Nedela':
+        console.log('weekend');
+        break;
+      default:
+         console.log('unnone');
+ 
+}
+
+
+
