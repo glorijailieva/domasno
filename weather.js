62a6a3be4c6a9c123c687d87a1c6e02388593ebf
glorijailieva <81261518+glorijailieva@users.noreply.github.com>
2021-06-07 21:00:36 +0200

    Add files via upload

---
+++ weather.js
@@ -0,0 +1,28 @@
+let input = document.querySelector('.name');
+let temp = document.querySelector('.temp');
+let desc = document.querySelector('.mesto');
+let button= document.querySelector('.button');
+input.style.width='450px';
+input.style.height='40px';
+button.style.height='45px';
+button.style.width='100px';
+
+button.addEventListener('click',weatherfunc=()=>{
+fetch('https://api.openweathermap.org/data/2.5/weather?q='+input.value+'&units=metric&appid=b6810aa34bf17f8f437b12819a4edadc&lang=mk')
+.then(response => response.json())
+.then(data => {
+  let tempvalue= data['main']['temp'];
+   let descvalue= data['weather'][0]['description'];
+  let description=descvalue.toUpperCase();
+  desc.style.color='green';
+  desc.style.padding='50px';
+  desc.innerHTML=description;
+  temp.style.color='green';
+  temp.innerHTML= "Температура: " + tempvalue.toFixed(0) + " C, Чувство: " + ' C' ;
+})
+.catch(error => alert("Wrong city name !"));
+});
+document.body.style.backgroundColor="black";
+document.body.style.fontSize="40px";
+document.body.style.marginTop="30px";
+document.body.style.textAlign="center";
